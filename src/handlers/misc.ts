25833f4b94960c4bc5e0c1393cb7e7b9d72263dd
Evan Abbott <evanabbott04@gmail.com>
2026-03-08 14:23:04 -0400

    added hail mairy error handling

--- src/handlers/misc.ts
+++ src/handlers/misc.ts
@@ -8,186 +8,219 @@ import { resolve } from "path";
 
 
 export async function getImages(req: Request, res: Response) {
-    await dbConnect();
-    if (!req.query._id) {
-        // could have a reviewId
-        if (req.query.reviewId) {
-            const images = (await Image.find({
-                reviewId: req.query.reviewId
-            })) as IImage[];
-            res.send({
-                error: false,
-                message: `Successfully retrieved ${images.length} image(s).`,
-                value: images
-            });
-
+    try {
+        await dbConnect();
+        if (!req.query._id) {
+            // could have a reviewId
+            if (req.query.reviewId) {
+                const images = (await Image.find({
+                    reviewId: req.query.reviewId
+                })) as IImage[];
+                res.send({
+                    error: false,
+                    message: `Successfully retrieved ${images.length} image(s).`,
+                    value: images
+                });
+    
+            } else {
+                // if no specified ID, just get all documents
+                const images = (await Image.find()) as IImage[];
+                res.send({
+                    error: false,
+                    message: `Successfully retrieved ${images.length} image(s).`,
+                    value: images
+                });
+            }
+    
         } else {
-            // if no specified ID, just get all documents
-            const images = (await Image.find()) as IImage[];
-            res.send({
-                error: false,
-                message: `Successfully retrieved ${images.length} image(s).`,
-                value: images
-            });
+            // if specified ID, fetch either document or image file
+            let image = (await Image.findById(req.query._id)) as IImage;
+            if (!image) {
+                res.status(400).send({
+                    error: true,
+                    message: `Invalid _id query parameter.`
+                });
+                return;
+            }
+    
+            if (req.query.doc && req.query.doc == "true") {
+                res.send({
+                    error: false,
+                    message: `Successfully retrieved image.`,
+                    value: image
+                });
+                return;
+                
+            } else {
+                // get image file
+                res.type(image.extension).sendFile(resolve(`images/${req.query._id}`));
+            }
         }
 
-    } else {
-        // if specified ID, fetch either document or image file
-        let image = (await Image.findById(req.query._id)) as IImage;
-        if (!image) {
-            res.status(400).send({
-                error: true,
-                message: `Invalid _id query parameter.`
-            });
-            return;
-        }
-
-        if (req.query.doc && req.query.doc == "true") {
-            res.send({
-                error: false,
-                message: `Successfully retrieved image.`,
-                value: image
-            });
-            return;
-            
-        } else {
-            // get image file
-            res.type(image.extension).sendFile(resolve(`images/${req.query._id}`));
-        }
+    } catch (e) {
+        const err = e as { message?: string };
+        res.send({
+            error: true,
+            message:
+                err.message != undefined ? err.message : "Failed to fetch image.",
+            value: undefined,
+        });
     }
 };
 
 export async function postImage(req: Request, res: Response) {
-    // console.log(`req.files: ${JSON.stringify(req.files)}`);
-    // console.log(`req.params: ${JSON.stringify(req.query)}`);
-    // console.log(`req.body: ${JSON.stringify(req.body)}`);
-
-    if (!req.body || !req.body.password) {
-        res.status(400).send({
-            error: true,
-            message: "No password field in body",
-        });
-        return;
-    }
-
-    if (req.body.password !== process.env.PASSWORD) {
-        res.status(400).send({
-            error: true,
-            message: "Incorrect password",
-        });
-        return;
-    }
-
-    if (!req.files || !req.files.images) {
-        res.status(400).send({
-            error: true,
-            message: "No files uploaded",
-        });
-        return;
-    }
+    try {
+        // console.log(`req.files: ${JSON.stringify(req.files)}`);
+        // console.log(`req.params: ${JSON.stringify(req.query)}`);
+        // console.log(`req.body: ${JSON.stringify(req.body)}`);
     
-    // messing around with types, ending with array of images
-    let uploaded: fileUpload.UploadedFile[];
-    if ((req.files as any).length) {
-        uploaded = req.files.images as fileUpload.UploadedFile[];
-    } else {
-        uploaded = [req.files.images] as fileUpload.UploadedFile[];
-    }
-    
-    // add each image
-    await dbConnect();
-    const newImages: IImage[] = [];
-    for (let i = 0; i < uploaded.length; ++i) {
-        // determine image size and type
-        const size = imageSize(uploaded[i].data);
-        if (!size.type) continue;
-
-        // create MongoDB image object
-        const toAdd: IImage = {
-            reviewId: req.body.reviewId,
-            md5: uploaded[i].md5,
-            name: uploaded[i].name,
-            extension: size.type,
-            size: uploaded[i].size,
-            width: size.width,
-            height: size.height
-        };
-
-        // upload object to database
-        const added = (await Image.insertOne(toAdd)) as IImage;
-        newImages.push(added);
-
-        // create file
-        writeFileSync("images/" + added._id!, uploaded[i].data);
-    }
-
-    res.send({
-        error: false,
-        message: `Successfully uploaded ${newImages.length} file(s)`,
-        value: newImages
-    });
-};
-
-
-export async function deleteImage(req: Request, res: Response) {
-    // console.log(`req.params: ${JSON.stringify(req.query)}`);
-
-    if (!req.body || !req.body.password) {
-        res.status(400).send({
-            error: true,
-            message: "No password field in body",
-        });
-        return;
-    }
-
-    if (req.body.password !== process.env.PASSWORD) {
-        res.status(400).send({
-            error: true,
-            message: "Incorrect password",
-        });
-        return;
-    }
-
-    if (!req.query._id && !req.query.reviewId) {
-        res.status(400).send({
-            error: true,
-            message: "Missing _id or reviewId query parameter"
-        });
-        return;
-    }
-
-    await dbConnect();
-    if (req.query._id) {
-        const deleted = await Image.findByIdAndDelete(req.query._id);
-        if (!deleted) {
+        if (!req.body || !req.body.password) {
             res.status(400).send({
                 error: true,
-                message: `Invalid _id query parameter`,
+                message: "No password field in body",
             });
             return;
         }
     
-        rmSync(`images/${req.query._id}`);
+        if (req.body.password !== process.env.PASSWORD) {
+            res.status(400).send({
+                error: true,
+                message: "Incorrect password",
+            });
+            return;
+        }
+    
+        if (!req.files || !req.files.images) {
+            res.status(400).send({
+                error: true,
+                message: "No files uploaded",
+            });
+            return;
+        }
+        
+        // messing around with types, ending with array of images
+        let uploaded: fileUpload.UploadedFile[];
+        if ((req.files as any).length) {
+            uploaded = req.files.images as fileUpload.UploadedFile[];
+        } else {
+            uploaded = [req.files.images] as fileUpload.UploadedFile[];
+        }
+        
+        // add each image
+        await dbConnect();
+        const newImages: IImage[] = [];
+        for (let i = 0; i < uploaded.length; ++i) {
+            // determine image size and type
+            const size = imageSize(uploaded[i].data);
+            if (!size.type) continue;
+    
+            // create MongoDB image object
+            const toAdd: IImage = {
+                reviewId: req.body.reviewId,
+                md5: uploaded[i].md5,
+                name: uploaded[i].name,
+                extension: size.type,
+                size: uploaded[i].size,
+                width: size.width,
+                height: size.height
+            };
+    
+            // upload object to database
+            const added = (await Image.insertOne(toAdd)) as IImage;
+            newImages.push(added);
+    
+            // create file
+            writeFileSync("images/" + added._id!, uploaded[i].data);
+        }
     
         res.send({
             error: false,
-            message: "Successfully deleted image",
-            value: deleted
+            message: `Successfully uploaded ${newImages.length} file(s)`,
+            value: newImages
         });
+        
+    } catch (e) {
+        const err = e as { message?: string };
+        res.send({
+            error: true,
+            message:
+                err.message != undefined ? err.message : "Failed to upload image.",
+            value: undefined,
+        });
+    }
+};
 
-    } else {
-        const toDelete = await Image.find({ reviewId: req.query.reviewId }) as IImage[];
-        toDelete.forEach(val => {
-            (async () => {
-                await Image.findByIdAndDelete(val._id);
-                rmSync(`images/${val._id}`);
-            })();
-        });
+
+export async function deleteImage(req: Request, res: Response) {
+    try {
+        // console.log(`req.params: ${JSON.stringify(req.query)}`);
     
+        if (!req.body || !req.body.password) {
+            res.status(400).send({
+                error: true,
+                message: "No password field in body",
+            });
+            return;
+        }
+    
+        if (req.body.password !== process.env.PASSWORD) {
+            res.status(400).send({
+                error: true,
+                message: "Incorrect password",
+            });
+            return;
+        }
+    
+        if (!req.query._id && !req.query.reviewId) {
+            res.status(400).send({
+                error: true,
+                message: "Missing _id or reviewId query parameter"
+            });
+            return;
+        }
+    
+        await dbConnect();
+        if (req.query._id) {
+            const deleted = await Image.findByIdAndDelete(req.query._id);
+            if (!deleted) {
+                res.status(400).send({
+                    error: true,
+                    message: `Invalid _id query parameter`,
+                });
+                return;
+            }
+        
+            rmSync(`images/${req.query._id}`);
+        
+            res.send({
+                error: false,
+                message: "Successfully deleted image",
+                value: deleted
+            });
+    
+        } else {
+            const toDelete = await Image.find({ reviewId: req.query.reviewId }) as IImage[];
+            toDelete.forEach(val => {
+                (async () => {
+                    await Image.findByIdAndDelete(val._id);
+                    rmSync(`images/${val._id}`);
+                })();
+            });
+        
+            res.send({
+                error: false,
+                message: `Successfully deleted ${toDelete.length} image(s)`,
+                value: toDelete
+            });
+        }
+
+    } catch (e) {
+        const err = e as { message?: string };
         res.send({
-            error: false,
-            message: `Successfully deleted ${toDelete.length} image(s)`,
-            value: toDelete
+            error: true,
+            message:
+                err.message != undefined ? err.message : "Failed to delete image.",
+            value: undefined,
         });
     }
 };
